be610f87d20ac317e5e783ca80f2da00dd0a0e26
lel99999 <luan.le@cfpb.gov>
2019-10-23 23:57:41 -0400

    upd setup env

--- setGOenv.sh
+++ setGOenv.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-export GOPATH=$HOME/go
+#export GOPATH=$HOME/go
+export GOPATH=$HOME/.go
 
-export GOROOT=$HOME/go1.X
-export PATH=$PATH:$GOROOT/bin
+#export GOROOT=$HOME/go1.X
+export GOROOT=/usr/local/opt/go/libexec
+export PATH="$PATH:${GOPATH}/bin:${GOROOT}/bin"
